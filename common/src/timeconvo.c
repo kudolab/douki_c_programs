03eebe4540b6e22c2040256a984db11c68642c64
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2020-05-17 11:41:22 +0900

    Fix: free文を追加

--- common/src/timeconvo.c
+++ common/src/timeconvo.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "../../include/FileLib_osx.c"
 
+#include "../../include/FileLib_osx.c"
 
 void help(void) {
     fprintf(stderr, "> \t Linear convolution  Ver.2.0  by Y.Shimazu ");
@@ -11,9 +11,7 @@ void help(void) {
     exit(EXIT_FAILURE);
 }
 
-
 int main(int argc, char **argv) {
-
     int n, p, i;
     int N1, N2;
     double *x, *h, *y;
@@ -26,9 +24,9 @@ int main(int argc, char **argv) {
     N1 = lenfile(argv[1]);
     N2 = lenfile(argv[2]);
 
-    x = (double *) calloc(N1, (size_t) sizeof(double));
-    h = (double *) calloc(N2, (size_t) sizeof(double));
-    y = (double *) calloc(N1 + N2, (size_t) sizeof(double));
+    x = (double *)calloc(N1, (size_t)sizeof(double));
+    h = (double *)calloc(N2, (size_t)sizeof(double));
+    y = (double *)calloc(N1 + N2, (size_t)sizeof(double));
 
     fprintf(stderr, "> calculate convolution...\n");
     fprintf(stderr, "> signal length  %d / %d\n", N1, N2);
@@ -39,20 +37,19 @@ int main(int argc, char **argv) {
     AnyFileRead(argv[2], h, N2);
     fprintf(stderr, " OK. \n");
 
-
     /* calculating convolution 69 */
     for (p = 0; p < N1; p++) {
-        fprintf(stderr, "%2d [%%]", (int) ((double) p / (double) N1 * 100.0 + 0.5));
+        fprintf(stderr, "%2d [%%]", (int)((double)p / (double)N1 * 100.0 + 0.5));
         for (n = p; n < N2 + p; n++) {
             y[n] += x[p] * h[n - p];
         }
         fprintf(stderr, "\b\b\b\b\b\b");
     }
 
-
     /*output data y[n]*/
     AnyFileWrite(argv[3], y, N1 + N2);
+    free(x);
+    free(h);
+    free(y);
     fprintf(stderr, "> Completed.\n");
-
 }
-
