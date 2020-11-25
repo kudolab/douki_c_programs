79ca0484f26cff150b805f0fff2e82bdd6510101
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2020-11-25 22:08:26 +0900

    Add hint comment

--- dv.c
+++ dv.c
@@ -9,6 +9,7 @@ int main(int argc, char *argv[]) {
 
     if (argc != 3) {
         fprintf(stderr, "An argument is insufficient\n");
+        fprintf(stderr, "dv converts datatype with specified filename extention\n");
         fprintf(stderr, "Usage: dv input(.DXX) output(.DXX)\n");
         exit(EXIT_FAILURE);
     }
@@ -16,7 +17,7 @@ int main(int argc, char *argv[]) {
     length = lenfile(argv[1]);
     x = (double *)malloc(length * sizeof(double));
     if (x == NULL) {
-        fprintf(stderr, "memory allocation failed at calloc()\n");
+        fprintf(stderr, "memory allocation failed at malloc()\n");
         exit(EXIT_FAILURE);
     }
 
